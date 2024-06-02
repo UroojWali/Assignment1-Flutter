8e6cd7c6044aaaf4e103cb87a31e9478f4e11414
uroojfatimawali <uroojfatimawali@gmail.com>
2024-06-02 06:41:00 +0000

    Assignment completed

---
+++ lib/homepage.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column (
+        children: [
+       Text('Urooj Fatima '),
+       Text('skill'),
+       Text ('Hobbies'),
+       Text  ('Student' ),
+
+
+        ],
+    ),);
+  }
+}
